973c242199a4bab8aa043c3282706dcaa48cd0c3
Tim Bingtan <tbingtan@uci.edu>
2022-02-08 22:25:58 -0800

    Updated descriptions in the Experience section

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 //$(".cloud").animate({left: '2000px'}, 10000);
 
 
-
+// Changing navbar color based on scroll area
 document.addEventListener('scroll', () => {
   document.documentElement.dataset.scroll = window.scrollY;
 });
